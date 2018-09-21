4c54366c2aa8962688e435320b91a4861583359e
Benoît Ganne <bganne@kalrayinc.com>
2018-09-21 03:40:18 +0200

    improve provisioning & readme

--- target-provision.sh
+++ target-provision.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-yum install -y /vagrant/kernel-4.18.0_rc6+-1.x86_64.rpm
-grubby --set-default=/boot/vmlinuz-4.18.0-rc6+
+yum install -y /vagrant/kernel-*.rpm
+grubby --set-default="$(rpm -qpl /vagrant/kernel-*.rpm|grep /boot/vmlinuz)"
 reboot
